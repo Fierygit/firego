2c7ba09cfb5a2070d39f1e313a1f682ad32be7a1
fireflychen <fireflychen@tencent.com>
2020-08-23 14:03:34 +0800

    update

--- src/chatroom/room.go
+++ src/chatroom/room.go
@@ -1,5 +1,7 @@
 package chatroom
 
+import "time"
+
 type Client struct {
 	IP            string // 用户的ip
 	Name          string // 用户的昵称， 一个要一个， 前段可以随机生成
@@ -11,24 +13,75 @@ type Client struct {
 type Message struct {
 	Type     int         // 消息类型， 0 为普通消息， 1 为提示消息等等
 	Info     interface{} // 存储消息
-	SendTime int64       // 存储这条消息的时间
+	SendTime time.Time       // 存储这条消息的时间
 }
 
 type RoomInfo struct {
-	CreateInfo CreateRoomArg // 创建房间必要的参数
-	CreateTime int64         // 创建的时间
+	CreateInfo *CreateRoomArg // 创建房间必要的参数
+	CreateTime time.Time      // 创建的时间
 }
 
 type Room struct {
 	RoomInfo  RoomInfo
-	ClientNum int               //	在线的人数
-	Messages  []Message         // 	消息列表， 索引号按时间排序
-	Clients   map[string]string //	SessionId -> Client
+	ClientNum int                //	在线的人数
+	Messages  []*Message         // 	消息列表， 索引号按时间排序
+	Clients   map[string]*Client //	SessionId -> Client
 }
 
 // chatroom 的结构体
 // 暂时不支持通过房间名字查询
 type ChatRoom struct {
-	RoomNum int             // 房间的个数
-	Rooms   map[string]Room // uuid -> Room
+	Rooms map[string]*Room // uuid -> Room
+}
+
+// -------------------------- operation -------------------------------------
+// --------------------------------Room-------------------------------
+func (room *Room) fooBar() {
+
+}
+
+func (room *Room) addMessage() {
+
+}
+
+func (room *Room) getMessage(start int, end int) {
+
+}
+
+func (room *Room) addClient() {
+
+}
+
+func (room *Room) removeClient() {
+
+}
+
+//---------------------------chatRoom-------------------------------------
+func InitChatRoom() *ChatRoom {
+	return &ChatRoom{map[string]*Room{}}
+}
+
+// 当已经存在房间了放回false
+// todo 冲突，还是重复了？
+func (chatRoom *ChatRoom) AddRoom(uuid string, createRoomArg *CreateRoomArg) bool {
+	if chatRoom.Rooms[uuid] != nil {
+		return false
+	}
+	room := Room{
+		RoomInfo: RoomInfo{
+			createRoomArg,
+			time.Now()},
+		ClientNum: 0,
+		Messages:  []*Message{},
+		Clients:   map[string]*Client{}}
+	chatRoom.Rooms[uuid] = &room
+	return true
+}
+
+func (chatRoom *ChatRoom) RemoveRoom(uuid string) {
+	chatRoom.Rooms[uuid] = nil
+}
+
+func (chatRoom *ChatRoom) RoomNum() int {
+	return len(chatRoom.Rooms)
 }

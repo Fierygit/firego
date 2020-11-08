1cdac9a41ecd23db9071f5929d75bcbba72b157f
HNU-CSEE-Nuwa <hnucessanonym@outlook.com>
2020-11-08 15:18:36 +0800

    update

---
+++ src/beibei/weibo.go
@@ -0,0 +1,40 @@
+/*
+ * @Author: Firefly
+ * @Date: 2020-11-08 14:51:57
+ * @Descripttion:
+ * @LastEditTime: 2020-11-08 15:18:06
+ */
+package beibei
+
+import (
+	"encoding/json"
+	"firego/src/common/response"
+	"fmt"
+	"io/ioutil"
+	"reflect"
+
+	"github.com/gin-gonic/gin"
+)
+
+//GetTest v
+func GetTest(ctx *gin.Context) {
+	b, err1 := ioutil.ReadFile("beibei/data/dealed.json")
+	if err1 != nil {
+		fmt.Println("read fail", err1)
+	}
+	var m interface{}
+
+	err := json.Unmarshal(b, &m)
+	if err != nil {
+		fmt.Println("Umarshal failed:", err)
+	}
+
+	// for k, v := range m {
+	// 	fmt.Println(k, ":", v)
+	// 	break
+	// }
+	fmt.Print(reflect.TypeOf(m))
+	test := m.(map[string]interface{})
+	response.Success(ctx, test, "success")
+
+}
